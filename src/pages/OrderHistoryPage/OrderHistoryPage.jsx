cb65ee8399ea954f9398283bcd6f4641f250bc3c
Danielle Martinez <daniellealexandria10@gmail.com>
2021-05-06 15:15:21 -0600

    order array

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './OrderHistoryPage.css';
 import * as usersService from '../../utilities/users-service';
+import * as ordersAPI from '../../utilities/orders-api';
 import Logo from '../../components/Logo/Logo';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
 export default function OrderHistoryPage({ user, setUser }) {
+  const [completedOrders, setCompletedOrders] = useState([]);
 
-
+  useEffect(function() {
+    async function getOrders() {
+      const orders = await ordersAPI.getAll();
+      const paidOrders = orders.filter(order => order.isPaid);
+      setCompletedOrders(paidOrders);
+    }
+    getOrders(); 
+  
+  }, []);
   return (
     <main className="OrderHistoryPage">
       <aside>
--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -2,6 +2,10 @@ import sendRequest from './send-request';
 
 const BASE_URL = '/api/orders';
 
+export function getAll() {
+  return sendRequest(BASE_URL);
+}
+
 // Retrieve an unpaid order for the logged in user
 export function getCart() {
   return sendRequest(`${BASE_URL}/cart`);
